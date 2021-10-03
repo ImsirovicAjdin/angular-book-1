06906fd03e7c29eb44721d0be462253637924f59
Ajdin Imsirovic <ajdika@live.com>
2021-10-03 16:50:19 +0200

    5.4 Binding innerHTML property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  yearTitle = 'The year';
   title = 'my-app';
   //border = this.randomWidth();
   border = 0;
