eea75adaafc9c8a32914f1faa6cfc2be1fe0c9f7
Ajdin Imsirovic <ajdika@live.com>
2021-10-03 09:01:20 +0200

    5.2 Dynamically update html attributes using property binding and method calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
-  border = 10;
+  border = this.randomWidth();
+
+  constructor() {
+    console.log(this.randomWidth());
+  }
 
   randomColor() {
     let hex = '#'+Math.random().toString(16).substr(-6);
     alert(`This is a random color: ${hex}`);
+  }
+
+  randomWidth() {
+    return Math.round(Math.random()*100);
   }
 }
